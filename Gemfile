8152f1eb720ae7d1c41a7fa74b0648f7977d3592
George H <georgehotarek@gmail.com>
2011-07-11 15:11:55 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'sqlite3', :group => :development
+gem 'gravatar_image_tag', '~> 0.1.0'
+gem 'will_paginate', '~> 3.0.pre2'
+gem 'money'
+
+group :development do
+	gem "rspec-rails", "~> 2.6.0"
+	gem "annotate", "~> 2.4.0"
+	gem 'faker', '0.3.1'
+end
+
+group :test do
+	gem "rspec", "~> 2.6.0"
+	gem "spork", "~> 0.9.0.rc"
+	gem "webrat"
+	gem "factory_girl_rails", "~> 1.0"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showroom::Application.config.secret_token = 'ac632e869e86319c4c7044b619e6ddd4583c2a21669bdc14e5ad7c496260c0304168f3e9dee1cdae6c51d39462205ae7ed86cb290297f0435bc4085215e1824e'
